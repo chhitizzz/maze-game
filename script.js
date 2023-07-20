3435fc4a47c485585d1acdcd3fadc3dd9a85e939
ChhitizV <chhitizvaidya10@gmail.com>
2023-07-20 13:51:47 +0545

    js

--- script.js
+++ script.js
@@ -16,4 +16,15 @@ function changeBrightness(factor, sprite) {
     virtCanvas.height = 500;
     var context = virtCanvas.getContext("2d");
     context.drawImage(sprite, 0, 0, 500, 500);
+
+    var imgData = context.getImageData(0, 0, 500, 500);
+
+    for(let i = 0; i < imgData.data.length; i += 4) {
+        imgData.data[i] = imgData.data[i] * factor;
+        imgData.data[i + 1] = imgData.data[i + 1] * factor;
+        imgData.data[i + 2] = imgData.data[i + 2] * factor;
+    }
+    context.putImageData(imgData, 0, 0);
+
+    
 }
